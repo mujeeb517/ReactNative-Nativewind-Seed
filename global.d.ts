94da7c7e77fa24fcc60a66b53b47c265326312a0
Mujib <mujib@Mujibs-MacBook-Pro-2.local>
2025-05-01 11:56:57 +0530

    tailwind incomplete integration

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+// global.d.ts
+declare module 'react-native' {
+    interface ViewProps {
+        className?: string;
+    }
+    interface TextProps {
+        className?: string;
+    }
+}
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,12 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { withNativeWind } = require("nativewind/metro")
+const path = require('path');
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = getDefaultConfig(__dirname)
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = withNativeWind(config, { input: "./global.css" })
